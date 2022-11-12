60dbc39fe04dda587f64c33ee4dd8a3ddae31142
carlo <carlostoribioc.1@gmail.com>
2022-11-12 15:57:56 -0400

    new file:   src/Nav.css 	modified:   src/Nav.jsx 	new file:   src/index.css 	modified:   src/index.js

---
+++ src/Nav.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './Nav.css'
+
+export function Nav(){
+    <div className='bg'>
+        <h1 className='bg'>Hello Word</h1>
+    </div>
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM  from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function Ejemplo(){
-    return <h1>Hello World</h1>
+function Ejemplo() {
+  return <h1>Hello World</h1>;
 }
 
 root.render(
-    <div>
-        <Ejemplo/>
-    </div>
-)
+  <div>
+    <Ejemplo />
+  </div>
+);
